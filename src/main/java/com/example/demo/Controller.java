ea3ab7b0ec75d3a85c0036211ad71a4eb2fbffd3
George Simoglou <gskyknight@outlook.com>
2023-01-27 18:06:49 +0000

    fix controller

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,3 +1,9 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping(value="/api")
 public class Controller {
